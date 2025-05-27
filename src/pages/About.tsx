9dc7820981149a66bad8d3f2b64d27dd1cd9500d
Eivakariuki <evalkariuki@gmail.com>
2025-05-27 21:33:08 +0300

    edit layout

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -18,34 +18,34 @@ const About: React.FC = () => {
   };
 
   return (
-    <div>
-      <div className="relative">
+    <div className="bg-[#f1f2fc]">
+      <div className="relative ">
         <HeroAbout />
         {/* Sticky Navigation */}
-        <div className="absolute bottom-0 left-0 right-0 z-30 transform translate-y-1/2">
-          <div className="bg-[#f1f2fc] shadow-md rounded-xl px-4 md:px-8 py-4 md:py-6 text-sm md:text-base mx-4 md:mx-8">
-            <div className="flex flex-col md:flex-row justify-center items-center md:space-x-8 space-y-4 md:space-y-0">
+        <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/2 z-10 w-full max-w-md md:max-w-4xl px-4">
+          <div className="bg-[#f1f2fc] shadow-md rounded-xl px-4 md:px-8 py-4 md:py-6 text-sm md:text-base">
+            <div className="grid grid-cols-2 gap-3 md:flex md:justify-center md:items-center md:space-x-8">
               <button 
                 onClick={() => scrollToSection(missionRef)}
-                className="text-[#0e013d] hover:text-[#FF8C00] font-medium w-full md:w-auto text-center py-2 md:py-0"
+                className="text-[#0e013d] hover:text-[#F4b24a] font-medium w-full md:w-auto text-center py-2 md:py-0"
               >
                 Mission & Vision
               </button>
               <button 
                 onClick={() => scrollToSection(historyRef)}
-                className="text-[#0e013d] hover:text-[#FF8C00] font-medium w-full md:w-auto text-center py-2 md:py-0"
+                className="text-[#0e013d] hover:text-[#F4b24a] font-medium w-full md:w-auto text-center py-2 md:py-0"
               >
                 Our History
               </button>
               <button 
                 onClick={() => scrollToSection(leadershipRef)}
-                className="text-[#0e013d] hover:text-[#FF8C00] font-medium w-full md:w-auto text-center py-2 md:py-0"
+                className="text-[#0e013d] hover:text-[#F4b24a] font-medium w-full md:w-auto text-center py-2 md:py-0"
               >
                 Leadership
               </button>
               <button 
                 onClick={() => scrollToSection(whyChoosePioneerRef)}
-                className="text-[#0e013d] hover:text-[#FF8C00] font-medium w-full md:w-auto text-center py-2 md:py-0"
+                className="text-[#0e013d] hover:text-[#F4b24a] font-medium w-full md:w-auto text-center py-2 md:py-0"
               >
                 Why Choose Pioneer
               </button>
