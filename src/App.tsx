08a0bad56ee97e7302b9b7be1df1662714758647
Eivakariuki <evalkariuki@gmail.com>
2025-05-28 11:43:02 +0300

    remove old navigation bar, remove join us page

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import About from "./pages/About";
-import Navigation from "./components/Navigation";
+// import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
 import VisitUsPage from "./pages/VisitUs";
 import Admissions from "./pages/Admissions.tsx";
 import ContactPage from "./pages/ContactPage";
-import JoinUsPage from "./pages/JoinUsPage";
+// import JoinUsPage from "./pages/JoinUsPage";
 import EnrollmentProcess from "./components/Enrollment.tsx";
 import Academics from "./pages/Academics";
 import Navigation2 from "./components/Navigation2";
@@ -40,7 +40,7 @@ const App = () => {
             <Route path="/admissions" element={<Admissions />} />
             <Route path="/visit-us" element={<VisitUsPage />} />
             <Route path="/contact" element={<ContactPage />} />
-            <Route path="/join" element={<JoinUsPage />} />
+            <Route path="/join" element={<Admissions />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
           <ChatIcon isMenuOpen={isMenuOpen} />
